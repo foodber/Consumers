a35577b0dd88e3c33c676e51e735d24f2b1c71e2
kj192 <kxjo47@gmail.com>
2019-01-28 15:44:09 -0500

    erase-incoming-orders-component

--- screens/OrderPage.js
+++ screens/OrderPage.js
@@ -10,13 +10,13 @@ import {
 import { Constants } from 'expo';
 import * as firebase from 'firebase';
 
-const config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
+var config = {
+  apiKey: 'AIzaSyDluonuaPcLFWSjnA7h8EaRCKxZnUHJ19g',
+  authDomain: 'foodber-65c10.firebaseapp.com',
+  databaseURL: 'https://foodber-65c10.firebaseio.com',
+  projectId: 'foodber-65c10',
+  storageBucket: 'foodber-65c10.appspot.com',
+  messagingSenderId: '669394895252',
 };
 
 firebase.initializeApp(config);

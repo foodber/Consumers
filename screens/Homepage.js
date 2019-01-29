3045efc189c88e67b7ba588ecc73bf09839789db
Gerson Espinal <gerson.espinal@gmail.com>
2019-01-29 17:02:09 -0500

    Update Homepage.js

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -11,14 +11,7 @@ import {
 import { Constants } from "expo";
 import * as firebase from "firebase";
 
-var config = {
-  apiKey: "AIzaSyDluonuaPcLFWSjnA7h8EaRCKxZnUHJ19g",
-  authDomain: "foodber-65c10.firebaseapp.com",
-  databaseURL: "https://foodber-65c10.firebaseio.com",
-  projectId: "foodber-65c10",
-  storageBucket: "foodber-65c10.appspot.com",
-  messagingSenderId: "669394895252"
-};
+var config = {};
 
 firebase.initializeApp(config);
 

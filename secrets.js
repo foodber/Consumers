af4cdd4286a7885f831d0a166d3b19ac688a2e04
Kevin Cho <kevinhcho27@gmail.com>
2019-01-29 12:52:51 -0500

    merge master

---
+++ secrets.js
@@ -0,0 +1,6 @@
+process.env.apiKey = "AIzaSyDluonuaPcLFWSjnA7h8EaRCKxZnUHJ19g";
+process.env.authDomain = "foodber-65c10.firebaseapp.com";
+process.env.databaseURL = "https://foodber-65c10.firebaseio.com";
+process.env.projectId = "foodber-65c10";
+process.env.storageBucket = "foodber-65c10.appspot.com";
+process.env.messagingSenderId = "669394895252";

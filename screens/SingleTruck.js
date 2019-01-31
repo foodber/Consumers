37f4928c406fdc0bef90ee6a791753102993a2ef
kj192 <kxjo47@gmail.com>
2019-01-31 16:41:28 -0500

    refactor: remove the use of navigation params for getting menu

--- screens/SingleTruck.js
+++ screens/SingleTruck.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { ScrollView, StyleSheet, View, Text, Button } from "react-native";
-import * as firebase from "firebase";
-import { Constants } from "expo";
-import {fetchTruckMenu} from '../store/trucksReducer'
-import {connect} from 'react-redux'
+import React from 'react';
+import { ScrollView, StyleSheet, View, Text, Button } from 'react-native';
+import * as firebase from 'firebase';
+import { Constants } from 'expo';
+import { fetchTruckMenu } from '../store/trucksReducer';
+import { connect } from 'react-redux';
 
 class SingleTruck extends React.Component {
   constructor(props) {
@@ -11,85 +11,67 @@ class SingleTruck extends React.Component {
     this.state = {
       cart: [],
       quantity: 1,
-      truckName: ''
+      truckName: '',
     };
-    this.increaseQuantity = this.increaseQuantity.bind(this)
-    this.decreaseQuantity = this.decreaseQuantity.bind(this)
+    this.increaseQuantity = this.increaseQuantity.bind(this);
+    this.decreaseQuantity = this.decreaseQuantity.bind(this);
   }
-  
+
   static navigationOptions = {
     title: 'Truck',
     style: {
-      backgroundColor: "blue"
-    }
+      backgroundColor: 'blue',
+    },
   };
   async componentDidMount() {
     const truckKey = this.props.navigation.getParam(
-      "truckKey",
-      "Not Available"
+      'truckKey',
+      'Not Available'
     );
-      await this.props.fetchTruckMenu(truckKey)
+    await this.props.fetchTruckMenu(truckKey);
   }
 
   //TRY THESE WITH RENDERING THE STATE SOMEWHERE ELSE
   //ON THE PAGE
   increaseQuantity() {
-    this.setState({quantity: this.state.quantity + 1})
+    this.setState({ quantity: this.state.quantity + 1 });
   }
 
   decreaseQuantity() {
     if (this.state.quantity === 1) {
-      this.setState({quantity: 1})
+      this.setState({ quantity: 1 });
     } else {
-      this.setState({quantity: this.state.quantity - 1})
+      this.setState({ quantity: this.state.quantity - 1 });
     }
   }
- 
+
   render() {
-    const menu = this.props.menu || []
-    const value = Object.keys(menu)
+    const menu = this.props.menu || [];
+    console.log('menu', menu);
+    const value = Object.keys(menu);
     const truckKey = this.props.navigation.getParam(
-      "truckKey",
-      "Not Available"
-    )
+      'truckKey',
+      'Not Available'
+    );
     return (
       <View style={styles.container}>
-      <Text>{truckKey}
-      </Text>
-      <Text>Menu</Text>
-        {value.map(item => {
-            return (
-              <View key={item}>
-                <Text>Item : {item}</Text>
-                <Text>Price : {menu[item]}</Text>
-                <Button title="+" onPress={this.increaseQuantity} />
-                <Button title="-" onPress={this.decreaseQuantity} />
-                <Button
-                  title="Add To Cart"
-                  onPress={() => {
-                    this.setState({
-                      cart: [
-                        ...this.state.cart,
-                        { [item]: menu[item],
-                          quantity: this.state.quantity,
-                          truckName: truckKey
-                        }
-                      ],
-                      quantity: 1,
-                    });
-                  }}
-                />
-              </View>
-            );
-          })}
-          <Text>Quantity : {this.state.quantity} </Text>
+        <Text>{truckKey}</Text>
+        <Text>Menu</Text>
+        {menu.map((menuItem, index) => {
+          return (
+            <View key={index}>
+              <Text>Item: {menuItem.name}</Text>
+              <Text>Price: {menuItem.price}</Text>
+            </View>
+          );
+        })}
         <Button
           color="red"
           title="Proceed To Checkout"
           onPress={() => {
-            this.props.navigation.navigate("Cart", {
+            this.props.navigation.navigate('Cart', {
               cart: this.state.cart,
-              truckKey: this.state.truckName
+              truckKey: this.state.truckName,
             });
           }}
         />
@@ -102,28 +84,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: "#fff",
-    marginTop: Constants.statusBarHeight
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
   },
   isItWorking: {
-    fontSize: 24
+    fontSize: 24,
   },
   theHeader: {
     fontSize: 17,
-    color: "rgba(96,100,109, 1)",
+    color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
-    textAlign: "left"
-  }
+    textAlign: 'left',
+  },
 });
 
-const mapStateToprops = state => ({
-  menu: state.menu.menu
-})
+const mapStateToProps = state => ({
+  menu: state.allTrucks.menu,
+});
 
-const mapDispatchToProps = dispatch => ({
-  fetchTruckMenu: (key) => {
-    dispatch(fetchTruckMenu(key))
-  }
-})
+// const mapDispatchToProps = dispatch => ({
+//   setTruckMenu: (key) => {
+//     dispatch(setTruckMenu(key))
+//   }
+// })
 
-export default connect(mapStateToprops, mapDispatchToProps)(SingleTruck)
+export default connect(mapStateToProps)(SingleTruck);

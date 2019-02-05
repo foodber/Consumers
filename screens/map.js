ef80f5900a9e1ec0cade135d8441a25bd6334b55
kj192 <kxjo47@gmail.com>
2019-02-05 11:51:23 -0500

    map now renders all open food truck carts

--- screens/map.js
+++ screens/map.js
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import { Constants } from 'expo';
 import { connect } from 'react-redux';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { fetchTruckLocation } from '../store/mapReducer';
 import { truckLocation } from '../db/fire';
 
@@ -21,8 +21,13 @@ export default class MapTab extends React.Component {
       latitude: null,
       longitude: null,
       error: null,
+      markers: [],
     };
   }
+
+  static navigationOptions = {
+    title: 'Nearby Trucks',
+  };
 
   async componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -34,31 +39,59 @@ export default class MapTab extends React.Component {
         });
       },
       error => this.setState({ error: error.message }),
-      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 }
+      { enableHighAccuracy: false, timeout: 100000, maximumAge: 0 }
     );
 
     const coord = await truckLocation.get();
+    //console.log('HHHHHHHHH', coord.docs);
+    const finalArr = [];
     coord.docs.map(doc => {
-      console.log('*******************', doc.data());
+      const location = doc.data();
+      // console.log('*******************', doc.data());
+      finalArr.push(location.location);
     });
-
-    console.log('WOO>>>>>>', coordData);
+    this.setState({
+      markers: finalArr,
+    });
   }
 
   render() {
+    const latLong = this.state.markers || [];
+    console.log('asdfasdfasdfasdf', latLong);
     return (
       <View style={styles.container}>
         {this.state.latitude && this.state.longitude && (
           <MapView
             provider={PROVIDER_GOOGLE}
             style={styles.map}
+            showsUserLocation={true}
             initialRegion={{
               latitude: this.state.latitude,
               longitude: this.state.longitude,
-              latitudeDelta: 0.03,
-              longitudeDelta: 0.03,
+              longitudeDelta: 0.00299,
+              latitudeDelta: 0.00299,
             }}
-          />
+          >
+            {latLong.map(coordinates => {
+              return (
+                <MapView.Marker
+                  key={coordinates.Lat}
+                  coordinate={{
+                    longitude: coordinates.Long,
+                    latitude: coordinates.Lat,
+                  }}
+                />
+              );
+            })}
+            {/* <MapView.Circle
+              radius={200}
+              //strokeColor="transparent"
+              center={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude,
+              }}
+            /> */}
+          </MapView>
         )}
       </View>
     );
@@ -67,13 +100,15 @@ export default class MapTab extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
   },
-  map: { alignSelf: 'stretch', height: 450 },
+  map: { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 },
 });
 
 // const mapStateToProps = state => ({
@@ -90,3 +125,22 @@ const styles = StyleSheet.create({
 //   mapStateToProps,
 //   mapDispatchToProps
 // )(MapTab);
+
+// {/* <MapView.Marker
+//   coordinate={{
+//     longitude: -74.44363748958308,
+//     latitude: 40.49420049978771,
+//   }}
+// />
+// <MapView.Marker
+//   coordinate={{
+//     longitude: -74.44353748958998,
+//     latitude: 40.49450049978771,
+//   }}
+// />
+// <MapView.Marker
+//   coordinate={{
+//     longitude: -74.44353748958308,
+//     latitude: 40.49440049978771,
+//   }}
+// /> */}
